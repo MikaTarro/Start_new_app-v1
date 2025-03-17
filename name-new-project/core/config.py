a4a85e3fae58dc18025e7a854adac77c0d8390b2
Rainbow4 <becoool2013@mail.ru>
2025-03-17 15:23:35 +0700

    conf

--- name-new-project/core/config.py
+++ name-new-project/core/config.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
 
 
 class RunConfig(BaseModel):
@@ -12,9 +16,27 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False  # Убрали запятую
+    echo_pool: bool = False  # Убрали запятую
+    max_overflow: int = 10
+    pool_size: int = 50
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="STARTNEWAPP_V1__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig  # Убедитесь, что это поле будет передано
 
 
+# Создание экземпляра настроек
 settings = Settings()
+print(settings.db.url)
+
